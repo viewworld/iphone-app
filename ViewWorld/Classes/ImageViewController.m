e2cf475d656de2d70c1f452708248e46621a1d3d
Rune Persson <rbp@Rune-Perssons-Mac-mini.local>
2011-06-27 16:31:03 +0200

    Added function for select from camera roll - extra button for image selection Changed image to be shown in correct aspect ratio

--- ViewWorld/Classes/ImageViewController.m
+++ ViewWorld/Classes/ImageViewController.m
@@ -19,6 +19,8 @@
 	thisEntry.imageData = dataObj;
 }
 
+// Start the camera if the record image button is pressed
+
 -(void)imageButtonPressed{
 	if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == YES){
 		UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init]; 
@@ -34,15 +36,44 @@
 
 }
 
+// Open the camera roll if the "choose from camera roll" button is pressed
+
+-(void)rollButtonPressed{
+	if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum] == YES){
+		UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init]; 
+		imagePicker.delegate = self;
+		imagePicker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+		imagePicker.allowsEditing = NO;
+		
+		[self presentModalViewController:imagePicker animated:YES];
+		[imagePicker release];
+	}else {
+		NSLog(@"camera roll not available");
+	}
+    
+}
+
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+    
+    //Draw the button for choose from camera roll
+    UIButton *rollButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+	rollButton.frame = CGRectMake(10, 150, 300, 40);
+	[rollButton addTarget:self action:@selector(rollButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+	[rollButton setTitle:@"Choose from camera roll" forState:UIControlStateNormal];
+    
+    //Draw the button for starting camera
 	UIButton *imageButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 	imageButton.frame = CGRectMake(10, 100, 300, 40);
 	[imageButton addTarget:self action:@selector(imageButtonPressed) forControlEvents:UIControlEventTouchUpInside];
 	[imageButton setTitle:@"Start camera" forState:UIControlStateNormal];
 	
-	imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 150, 120, 160)];
+    //create a view for the recorded image
+	imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 200, 220, 160)];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
     
+    //fill in image data in the image image view
     Entry *thisEntry = [currentReport.entries objectAtIndex:index];
 	if (thisEntry.imageData != nil) {
 		UIImage *thisImage = [[UIImage alloc] initWithData:thisEntry.imageData];
@@ -52,6 +83,7 @@
 	
 	[self.view addSubview:imageView];
 	[self.view addSubview:imageButton];
+    [self.view addSubview:rollButton];
 	
     [super viewDidLoad];
 }
